84ce72c69290e02d8204f0710aae61172d1fa95b
Peter Donald <peter@realityforge.org>
2025-04-08 09:39:43 +1000

    Define new classes for different enemy types

---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Character/AuraCharacterBase.h"
+#include "CoreMinimal.h"
+#include "AuraCharacter.generated.h"
+
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+    GENERATED_BODY()
+};
---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Character/AuraCharacterBase.h"
+#include "CoreMinimal.h"
+#include "AuraEnemy.generated.h"
+
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+    GENERATED_BODY()
+};
