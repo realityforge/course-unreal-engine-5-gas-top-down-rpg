c704ca211bb565327911753e2986455f746d3e90
Peter Donald <peter@realityforge.org>
2025-04-13 21:01:43 +1000

    Add AuraGameModeBase, define blueprint subclass and make startup map use game mode

---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+    GENERATED_BODY()
+};
