85d134f6f5a6838ee78149f62d003bc1de4ede89
Peter Donald <peter@realityforge.org>
2025-04-13 20:09:46 +1000

    Define base class for player controller

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ *
+ */
+UCLASS(Abstract)
+class AURA_API AAuraPlayerController : public APlayerController
+{
+    GENERATED_BODY()
+};
