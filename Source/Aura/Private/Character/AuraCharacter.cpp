37f40e056abc221fd8768b12bf0a4761824accd4
Peter Donald <peter@realityforge.org>
2025-04-13 21:31:04 +1000

    Start to configure the camera boom so it is like a typical TopDown RPG

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -1,1 +1,32 @@
 #include "Character/AuraCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+    SpringArmComponent->SetupAttachment(GetRootComponent());
+    SpringArmComponent->TargetArmLength = 750.0f;
+    SpringArmComponent->bEnableCameraLag = true;
+
+    // Look down at the player character as it is a Top Down RPG
+    SpringArmComponent->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+
+    // We want to the camera to be fixed
+    SpringArmComponent->bInheritPitch = false;
+    SpringArmComponent->bInheritYaw = false;
+    SpringArmComponent->bInheritRoll = false;
+
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>("Camera");
+    CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+    GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
+    GetCharacterMovement()->bConstrainToPlane = true;
+    GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+    bUseControllerRotationPitch = false;
+    bUseControllerRotationYaw = false;
+    bUseControllerRotationRoll = false;
+}
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -4,8 +4,20 @@
 #include "CoreMinimal.h"
 #include "AuraCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS(Abstract)
 class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
     GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere)
+    TObjectPtr<UCameraComponent> CameraComponent{ nullptr };
+
+    UPROPERTY(EditAnywhere)
+    TObjectPtr<USpringArmComponent> SpringArmComponent{ nullptr };
+
+public:
+    AAuraCharacter();
 };
