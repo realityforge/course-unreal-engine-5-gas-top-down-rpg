ab8ed1f004f0d63f78a4d817494166eac61cd015
Peter Donald <peter@realityforge.org>
2025-04-13 20:29:54 +1000

    Configure input mode so that our mouse does not get hidden when captured by game frame

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -33,4 +33,11 @@ void AAuraPlayerController::BeginPlay()
     checkf(Subsystem, TEXT("EnhancedInputLocalPlayerSubsystem not enabled for project"));
 
     Subsystem->AddMappingContext(InputMappingContext, 0);
+
+    bShowMouseCursor = true;
+    DefaultMouseCursor = EMouseCursor::Default;
+    FInputModeGameAndUI InputModeData;
+    InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+    InputModeData.SetHideCursorDuringCapture(false);
+    SetInputMode(InputModeData);
 }
