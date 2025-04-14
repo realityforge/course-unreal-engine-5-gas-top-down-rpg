6344f2f944ee60fb9880cdbe34ac09da724d1218
Peter Donald <peter@realityforge.org>
2025-04-14 14:36:21 +1000

    Only apply validations for non-abstract classes

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -17,24 +17,28 @@ EDataValidationResult AAuraCharacterBase::IsDataValid(FDataValidationContext& Co
 {
     auto Result = CombineDataValidationResults(Super::IsDataValid(Context), EDataValidationResult::Valid);
 
-    const auto SkeletalMeshAsset = GetMesh()->GetSkeletalMeshAsset();
-    if (!GetClass()->HasAnyClassFlags(CLASS_Abstract) && !IsValid(SkeletalMeshAsset))
+    if (!GetClass()->HasAnyClassFlags(CLASS_Abstract))
     {
-        const auto String = FString::Printf(TEXT("Object %s is not an abstract class but has not specified "
-                                                 "the property Mesh.SkeletalMeshAsset"),
-                                            *GetActorNameOrLabel());
-        Context.AddError(FText::FromString(String));
-        Result = EDataValidationResult::Invalid;
-    }
-    else if (nullptr == GetMesh()->GetSocketByName(NAME_WeaponHandSocket))
-    {
-        const auto String = FString::Printf(TEXT("Object %s has Mesh component that references StaticMeshAsset %s "
-                                                 "but the asset does not have a socket named %s as expected"),
-                                            *GetActorNameOrLabel(),
-                                            *SkeletalMeshAsset->GetName(),
-                                            *NAME_WeaponHandSocket.ToString());
-        Context.AddError(FText::FromString(String));
-        Result = EDataValidationResult::Invalid;
+        // ReSharper disable once CppTooWideScopeInitStatement
+        const auto SkeletalMeshAsset = GetMesh()->GetSkeletalMeshAsset();
+        if (!IsValid(SkeletalMeshAsset))
+        {
+            const auto String = FString::Printf(TEXT("Object %s is not an abstract class but has not specified "
+                                                     "the property Mesh.SkeletalMeshAsset"),
+                                                *GetActorNameOrLabel());
+            Context.AddError(FText::FromString(String));
+            Result = EDataValidationResult::Invalid;
+        }
+        else if (nullptr == GetMesh()->GetSocketByName(NAME_WeaponHandSocket))
+        {
+            const auto String = FString::Printf(TEXT("Object %s has Mesh component that references StaticMeshAsset %s "
+                                                     "but the asset does not have a socket named %s as expected"),
+                                                *GetActorNameOrLabel(),
+                                                *SkeletalMeshAsset->GetName(),
+                                                *NAME_WeaponHandSocket.ToString());
+            Context.AddError(FText::FromString(String));
+            Result = EDataValidationResult::Invalid;
+        }
     }
 
     return Result;
