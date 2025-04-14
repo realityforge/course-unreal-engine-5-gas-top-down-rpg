0208ed1336e13cee7ef4f6e5350510f7f723c2a7
Peter Donald <peter@realityforge.org>
2025-04-15 09:19:27 +1000

    Remove unnecessary guards

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -12,20 +12,14 @@ void AAuraEnemy::HighlightActor()
 {
     GetMesh()->SetRenderCustomDepth(true);
     GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
-    if (Weapon)
-    {
-        Weapon->SetRenderCustomDepth(true);
-        Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
-    }
+    Weapon->SetRenderCustomDepth(true);
+    Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-    if (Weapon)
-    {
-        Weapon->SetRenderCustomDepth(false);
-        Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_CLEAR);
-    }
+    Weapon->SetRenderCustomDepth(false);
+    Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_CLEAR);
     GetMesh()->SetRenderCustomDepth(false);
     GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_CLEAR);
 }
