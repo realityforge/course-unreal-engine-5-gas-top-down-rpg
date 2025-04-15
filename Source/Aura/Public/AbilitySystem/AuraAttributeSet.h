1906f2e0f5bb8238b1d49addb0a7fb8153f7a72a
Peter Donald <peter@realityforge.org>
2025-04-15 15:04:03 +1000

    Add an initial set of attribute values

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -8,4 +8,33 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
     GENERATED_BODY()
+
+public:
+    UAuraAttributeSet();
+
+    UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing = OnRep_Health)
+    FGameplayAttributeData Health;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing = OnRep_MaxHealth)
+    FGameplayAttributeData MaxHealth;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing = OnRep_Mana)
+    FGameplayAttributeData Mana;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing = OnRep_MaxMana)
+    FGameplayAttributeData MaxMana;
+
+    UFUNCTION()
+    void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+    UFUNCTION()
+    void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
+    UFUNCTION()
+    void OnRep_Mana(const FGameplayAttributeData& OldMana);
+
+    UFUNCTION()
+    void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
+
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 };
