784196f043f35a6f8dd71f22d86c201b7cbcd98c
Peter Donald <peter@realityforge.org>
2025-04-30 22:25:12 +1000

    Override PostGameplayEffectExecute and start collecting some data from the effect

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -1,5 +1,8 @@
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
+#include "GameFramework/Character.h"
+#include "GameplayEffectExtension.h"
 #include "Net/UnrealNetwork.h"
 
 UAuraAttributeSet::UAuraAttributeSet()
@@ -48,6 +51,13 @@ void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, 
     }
 }
 
+void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+    Super::PostGameplayEffectExecute(Data);
+    FEffectProperties Props;
+    GetEffectProperties(Data, Props);
+}
+
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
     Super::GetLifetimeReplicatedProps(OutLifetimeProps);
@@ -56,4 +66,43 @@ void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
     DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeStatic
+void UAuraAttributeSet::GetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const
+{
+    // Source = Causer of the effect,
+    // Target = Target of the effect (and owner of this AttributeSet)
+
+    Props.EffectContextHandle = Data.EffectSpec.GetContext();
+    Props.SourceASC = Props.EffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
+
+    if (IsValid(Props.SourceASC) && Props.SourceASC->AbilityActorInfo.IsValid()
+        && Props.SourceASC->AbilityActorInfo->AvatarActor.IsValid())
+    {
+        Props.SourceAvatarActor = Props.SourceASC->AbilityActorInfo->AvatarActor.Get();
+        Props.SourceController = Props.SourceASC->AbilityActorInfo->PlayerController.Get();
+        if (!Props.SourceController && Props.SourceAvatarActor)
+        {
+            // If the ActorInfo has a null PlayerController then we fall back on trying to
+            // access controller from Avatar Actor.
+            // Not totally sure ... why ...
+            if (const auto Pawn = Cast<APawn>(Props.SourceAvatarActor))
+            {
+                Props.SourceController = Pawn->GetController();
+            }
+        }
+        if (Props.SourceController)
+        {
+            Props.SourceCharacter = Cast<ACharacter>(Props.SourceController->GetPawn());
+        }
+    }
+
+    if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
+    {
+        Props.TargetAvatarActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
+        Props.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+        Props.TargetCharacter = Cast<ACharacter>(Props.TargetAvatarActor);
+        Props.TargetASC = &Data.Target;
+    }
 }
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -11,6 +11,40 @@
     GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName)               \
     GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
+USTRUCT()
+struct FEffectProperties
+{
+    GENERATED_BODY()
+
+    FEffectProperties() {}
+
+    FGameplayEffectContextHandle EffectContextHandle;
+
+    UPROPERTY()
+    UAbilitySystemComponent* SourceASC{ nullptr };
+
+    UPROPERTY()
+    AActor* SourceAvatarActor{ nullptr };
+
+    UPROPERTY()
+    AController* SourceController{ nullptr };
+
+    UPROPERTY()
+    ACharacter* SourceCharacter{ nullptr };
+
+    UPROPERTY()
+    UAbilitySystemComponent* TargetASC{ nullptr };
+
+    UPROPERTY()
+    AActor* TargetAvatarActor{ nullptr };
+
+    UPROPERTY()
+    AController* TargetController{ nullptr };
+
+    UPROPERTY()
+    ACharacter* TargetCharacter{ nullptr };
+};
+
 UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
@@ -49,5 +83,11 @@ public:
 
     virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
 
+    virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+private:
+    // No idea why we do this at all here in attributes ...
+    void GetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const;
 };
