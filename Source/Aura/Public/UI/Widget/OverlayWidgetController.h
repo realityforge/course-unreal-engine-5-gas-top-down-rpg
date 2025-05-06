d41953381891ad12edad9c6b346a94faf2b7837f
Peter Donald <peter@realityforge.org>
2025-05-06 23:10:08 +1000

    Add util method

--- Source/Aura/Public/UI/Widget/OverlayWidgetController.h
+++ Source/Aura/Public/UI/Widget/OverlayWidgetController.h
@@ -68,4 +68,13 @@ private:
     void OnMaxManaChange(const FOnAttributeChangeData& OnAttributeChangeData) const;
 
     UAuraAttributeSet* GetAuraAttributeSet() const;
+
+    template <typename T>
+    static T* GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag);
 };
+
+template <typename T>
+T* UOverlayWidgetController::GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag)
+{
+    return DataTable->FindRow<T>(Tag.GetTagName(), TEXT(""));
+}
