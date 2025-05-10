719154a556e0f719d78039d6adb1042468733751
Peter Donald <peter@realityforge.org>
2025-05-11 09:08:09 +1000

    Declare secondar attribtues

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -17,25 +17,13 @@ UAuraAttributeSet::UAuraAttributeSet()
 // ReSharper disable once CppMemberFunctionMayBeConst
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
 {
-    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
-}
-
-// ReSharper disable once CppMemberFunctionMayBeConst
-void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
-{
-    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
+    GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Health, OldHealth);
 }
 
 // ReSharper disable once CppMemberFunctionMayBeConst
 void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana)
 {
-    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Mana, OldMana);
-}
-
-// ReSharper disable once CppMemberFunctionMayBeConst
-void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)
-{
-    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldMaxMana);
+    GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Mana, OldMana);
 }
 
 // ReSharper disable once CppMemberFunctionMayBeConst
@@ -60,6 +48,66 @@ void UAuraAttributeSet::OnRep_Resilience(const FGameplayAttributeData& OldResili
 void UAuraAttributeSet::OnRep_Vigor(const FGameplayAttributeData& OldVigor)
 {
     GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Vigor, OldVigor);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UAuraAttributeSet::OnRep_Armor(const FGameplayAttributeData& OldArmor)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, Armor, OldArmor);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UAuraAttributeSet::OnRep_ArmorPenetration(const FGameplayAttributeData& OldArmorPenetration)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, ArmorPenetration, OldArmorPenetration);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UAuraAttributeSet::OnRep_BlockChance(const FGameplayAttributeData& OldBlockChance)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, BlockChance, OldBlockChance);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UAuraAttributeSet::OnRep_CriticalHitChance(const FGameplayAttributeData& OldCriticalHitChance)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, CriticalHitChance, OldCriticalHitChance);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UAuraAttributeSet::OnRep_CriticalHitDamage(const FGameplayAttributeData& OldCriticalHitDamage)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, CriticalHitDamage, OldCriticalHitDamage);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UAuraAttributeSet::OnRep_CriticalHitResistance(const FGameplayAttributeData& OldCriticalHitResistance)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, CriticalHitResistance, OldCriticalHitResistance);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UAuraAttributeSet::OnRep_HealthRegeneration(const FGameplayAttributeData& OldHealthRegeneration)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, HealthRegeneration, OldHealthRegeneration);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UAuraAttributeSet::OnRep_ManaRegeneration(const FGameplayAttributeData& OldManaRegeneration)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(ThisClass, ManaRegeneration, OldManaRegeneration);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldMaxMana);
 }
 
 void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
@@ -96,13 +144,21 @@ void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
     Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
     DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, Health, COND_None, REPNOTIFY_Always);
-    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, MaxHealth, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, Mana, COND_None, REPNOTIFY_Always);
-    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, MaxMana, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, Strength, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, Intelligence, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, Resilience, COND_None, REPNOTIFY_Always);
     DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, Vigor, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, Armor, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, ArmorPenetration, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, BlockChance, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, CriticalHitChance, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, CriticalHitDamage, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, CriticalHitResistance, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, HealthRegeneration, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, ManaRegeneration, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, MaxHealth, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(ThisClass, MaxMana, COND_None, REPNOTIFY_Always);
 }
 
 // ReSharper disable once CppMemberFunctionMayBeStatic
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -57,45 +57,115 @@ public:
     FGameplayAttributeData Health;
     ATTRIBUTE_ACCESSORS(ThisClass, Health);
 
-    UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing = OnRep_MaxHealth)
-    FGameplayAttributeData MaxHealth;
-    ATTRIBUTE_ACCESSORS(ThisClass, MaxHealth);
-
     UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing = OnRep_Mana)
     FGameplayAttributeData Mana;
     ATTRIBUTE_ACCESSORS(ThisClass, Mana);
 
-    UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing = OnRep_MaxMana)
-    FGameplayAttributeData MaxMana;
-    ATTRIBUTE_ACCESSORS(ThisClass, MaxMana);
-
+    /** Attribute that increases Physical Damage. */
     UPROPERTY(BlueprintReadOnly, Category = "Primary Attributes", ReplicatedUsing = OnRep_Strength)
     FGameplayAttributeData Strength;
     ATTRIBUTE_ACCESSORS(ThisClass, Strength);
 
+    /** Attribute that increases Magical Damage. */
     UPROPERTY(BlueprintReadOnly, Category = "Primary Attributes", ReplicatedUsing = OnRep_Intelligence)
     FGameplayAttributeData Intelligence;
     ATTRIBUTE_ACCESSORS(ThisClass, Intelligence);
 
+    /** Attribute that increases Armor and Armor Penetration. */
     UPROPERTY(BlueprintReadOnly, Category = "Primary Attributes", ReplicatedUsing = OnRep_Resilience)
     FGameplayAttributeData Resilience;
     ATTRIBUTE_ACCESSORS(ThisClass, Resilience);
 
+    /** Attribute that increases Health. */
     UPROPERTY(BlueprintReadOnly, Category = "Primary Attributes", ReplicatedUsing = OnRep_Vigor)
     FGameplayAttributeData Vigor;
     ATTRIBUTE_ACCESSORS(ThisClass, Vigor);
+
+    /**
+     * Attribute that reduces damage taken and improves block chance.
+     * Depends on Resilience.
+     */
+    UPROPERTY(BlueprintReadOnly, Category = "Secondary Attributes", ReplicatedUsing = OnRep_Armor)
+    FGameplayAttributeData Armor;
+    ATTRIBUTE_ACCESSORS(ThisClass, Armor);
+
+    /**
+     * Attribute that ignores a percentage of enemy Armor and increases Critical Hit chance.
+     * Depends on Resilience.
+     */
+    UPROPERTY(BlueprintReadOnly, Category = "Secondary Attributes", ReplicatedUsing = OnRep_ArmorPenetration)
+    FGameplayAttributeData ArmorPenetration;
+    ATTRIBUTE_ACCESSORS(ThisClass, ArmorPenetration);
+
+    /**
+     * Attribute that indicates the chance to "block" and cut incoming damage in half.
+     * Depends on Armor.
+     */
+    UPROPERTY(BlueprintReadOnly, Category = "Secondary Attributes", ReplicatedUsing = OnRep_BlockChance)
+    FGameplayAttributeData BlockChance;
+    ATTRIBUTE_ACCESSORS(ThisClass, BlockChance);
+
+    /**
+     * Attribute that indicates the chance to "critical hit" and double damagae plus add a critical hit bonus.
+     * Depends on Armor Penetration.
+     */
+    UPROPERTY(BlueprintReadOnly, Category = "Secondary Attributes", ReplicatedUsing = OnRep_CriticalHitChance)
+    FGameplayAttributeData CriticalHitChance;
+    ATTRIBUTE_ACCESSORS(ThisClass, CriticalHitChance);
+
+    /**
+     * Attribute that indicates the damage added when a critical hit is scored.
+     * Depends on Armor Penetration.
+     */
+    UPROPERTY(BlueprintReadOnly, Category = "Secondary Attributes", ReplicatedUsing = OnRep_CriticalHitDamage)
+    FGameplayAttributeData CriticalHitDamage;
+    ATTRIBUTE_ACCESSORS(ThisClass, CriticalHitDamage);
+
+    /**
+     * Attribute that reduces the critical hit chance of attacking enemies.
+     * Depends on Armor.
+     */
+    UPROPERTY(BlueprintReadOnly, Category = "Secondary Attributes", ReplicatedUsing = OnRep_CriticalHitResistance)
+    FGameplayAttributeData CriticalHitResistance;
+    ATTRIBUTE_ACCESSORS(ThisClass, CriticalHitResistance);
+
+    /**
+     * Attribute that indicates the amount of health regenerated every 1 second.
+     * Depends on Vigor.
+     */
+    UPROPERTY(BlueprintReadOnly, Category = "Secondary Attributes", ReplicatedUsing = OnRep_HealthRegeneration)
+    FGameplayAttributeData HealthRegeneration;
+    ATTRIBUTE_ACCESSORS(ThisClass, HealthRegeneration);
+
+    /**
+     * Attribute that indicates the amount of mana regenerated every 1 second.
+     * Depends on Intelligence.
+     */
+    UPROPERTY(BlueprintReadOnly, Category = "Secondary Attributes", ReplicatedUsing = OnRep_ManaRegeneration)
+    FGameplayAttributeData ManaRegeneration;
+    ATTRIBUTE_ACCESSORS(ThisClass, ManaRegeneration);
+
+    /**
+     * Attribute that indicates the maximum amount of Health obtainable.
+     * Depends on Vigor.
+     */
+    UPROPERTY(BlueprintReadOnly, Category = "Secondary Attributes", ReplicatedUsing = OnRep_MaxHealth)
+    FGameplayAttributeData MaxHealth;
+    ATTRIBUTE_ACCESSORS(ThisClass, MaxHealth);
+
+    /**
+     * Attribute that indicates the maximum amount of Mana obtainable.
+     * Depends on Intelligence.
+     */
+    UPROPERTY(BlueprintReadOnly, Category = "Secondary Attributes", ReplicatedUsing = OnRep_MaxMana)
+    FGameplayAttributeData MaxMana;
+    ATTRIBUTE_ACCESSORS(ThisClass, MaxMana);
 
     UFUNCTION()
     void OnRep_Health(const FGameplayAttributeData& OldHealth);
 
     UFUNCTION()
-    void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
-
-    UFUNCTION()
     void OnRep_Mana(const FGameplayAttributeData& OldMana);
-
-    UFUNCTION()
-    void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
 
     UFUNCTION()
     void OnRep_Strength(const FGameplayAttributeData& OldStrength);
@@ -108,6 +178,36 @@ public:
 
     UFUNCTION()
     void OnRep_Vigor(const FGameplayAttributeData& OldVigor);
+
+    UFUNCTION()
+    void OnRep_Armor(const FGameplayAttributeData& OldArmor);
+
+    UFUNCTION()
+    void OnRep_ArmorPenetration(const FGameplayAttributeData& OldArmorPenetration);
+
+    UFUNCTION()
+    void OnRep_BlockChance(const FGameplayAttributeData& OldBlockChance);
+
+    UFUNCTION()
+    void OnRep_CriticalHitChance(const FGameplayAttributeData& OldCriticalHitChance);
+
+    UFUNCTION()
+    void OnRep_CriticalHitDamage(const FGameplayAttributeData& OldCriticalHitDamage);
+
+    UFUNCTION()
+    void OnRep_CriticalHitResistance(const FGameplayAttributeData& OldCriticalHitResistance);
+
+    UFUNCTION()
+    void OnRep_HealthRegeneration(const FGameplayAttributeData& OldHealthRegeneration);
+
+    UFUNCTION()
+    void OnRep_ManaRegeneration(const FGameplayAttributeData& OldManaRegeneration);
+
+    UFUNCTION()
+    void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
+    UFUNCTION()
+    void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
 
     virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
 
