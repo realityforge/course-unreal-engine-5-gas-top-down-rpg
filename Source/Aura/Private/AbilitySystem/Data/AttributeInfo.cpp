c4de9c7e1cc1b163fa3223f48c009345cfb59be4
Peter Donald <peter@realityforge.org>
2025-05-17 21:49:59 +1000

    Define assets for Attribute specs

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,84 @@
+﻿#include "AbilitySystem/Data/AttributeInfo.h"
+#include "Logging/StructuredLog.h"
+#include "Misc/DataValidation.h"
+
+FAuraAttributeDef UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag,
+                                                          const bool bLogNotFound) const
+{
+    for (const auto& Info : Attributes)
+    {
+        if (Info.Tag.MatchesTagExact(AttributeTag))
+        {
+            return Info;
+        }
+    }
+
+    if (bLogNotFound)
+    {
+        UE_LOGFMT(LogTemp,
+                  Error,
+                  "UAttributeInfo::FindAttributeInfoForTag() -"
+                  " Can't find Info for AttributeTag [{Tag}] on AttributeInfo [{Info}].",
+                  AttributeTag.GetTagName(),
+                  GetName());
+    }
+
+    return FAuraAttributeDef();
+}
+
+#if WITH_EDITOR
+EDataValidationResult UAttributeInfo::IsDataValid(FDataValidationContext& Context) const
+{
+    auto Result = CombineDataValidationResults(Super::IsDataValid(Context), EDataValidationResult::Valid);
+
+    if (!GetClass()->HasAnyClassFlags(CLASS_Abstract))
+    {
+        if (0 == Attributes.Num())
+        {
+            const auto String =
+                FString::Printf(TEXT("Object %s is not abstract but has specified 0 AttributeInformation values"),
+                                *GetName());
+            Context.AddError(FText::FromString(String));
+            Result = EDataValidationResult::Invalid;
+        }
+        else
+        {
+            int32 Index = 0;
+            // ReSharper disable once CppUseStructuredBinding
+            for (auto AttributeInfo : Attributes)
+            {
+                if (AttributeInfo.Tag.ToString().IsEmpty())
+                {
+                    const auto String =
+                        FString::Printf(TEXT("Object %s is not abstract but AttributeInformation[%d].Tag is empty."),
+                                        *GetName(),
+                                        Index);
+                    Context.AddError(FText::FromString(String));
+                    Result = EDataValidationResult::Invalid;
+                }
+                if (AttributeInfo.Name.ToString().IsEmpty())
+                {
+                    const auto String =
+                        FString::Printf(TEXT("Object %s is not abstract but AttributeInformation[%d].Name is empty"),
+                                        *GetName(),
+                                        Index);
+                    Context.AddError(FText::FromString(String));
+                    Result = EDataValidationResult::Invalid;
+                }
+                if (AttributeInfo.Description.ToString().IsEmpty())
+                {
+                    const auto String = FString::Printf(
+                        TEXT("Object %s is not abstract but AttributeInformation[%d].Description is empty"),
+                        *GetName(),
+                        Index);
+                    Context.AddError(FText::FromString(String));
+                    Result = EDataValidationResult::Invalid;
+                }
+                Index++;
+            }
+        }
+    }
+
+    return Result;
+}
+#endif
---
+++ Source/Aura/Public/AbilitySystem/Data/AttributeInfo.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "AttributeInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraAttributeDef
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "Attributes"))
+    FGameplayTag Tag{ FGameplayTag::EmptyTag };
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FText Name{};
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FText Description{};
+};
+
+USTRUCT(BlueprintType)
+struct FAuraAttributeInfo
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FAuraAttributeDef Def;
+
+    UPROPERTY(BlueprintReadOnly)
+    float Value{ 0.f };
+};
+
+UCLASS(Const)
+class AURA_API UAttributeInfo : public UDataAsset
+{
+    GENERATED_BODY()
+
+public:
+    FAuraAttributeDef FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound = false) const;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "{Name} {Tag}"))
+    TArray<FAuraAttributeDef> Attributes;
+
+#if WITH_EDITOR
+    virtual EDataValidationResult IsDataValid(FDataValidationContext& Context) const override;
+#endif
+};
