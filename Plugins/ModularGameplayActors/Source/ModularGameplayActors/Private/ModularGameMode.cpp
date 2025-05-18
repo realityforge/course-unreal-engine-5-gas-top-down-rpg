1c9ddc04bd358a849019bac68f3a207b9fec1e2d
Peter Donald <peter@realityforge.org>
2025-05-18 21:42:39 +1000

    Braid: Add mirror 'Plugins/ModularGameplayActors' at 'ccbf5c6'

---
+++ Plugins/ModularGameplayActors/Source/ModularGameplayActors/Private/ModularGameMode.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ModularGameMode.h"
+
+#include "ModularGameState.h"
+#include "ModularPawn.h"
+#include "ModularPlayerController.h"
+#include "ModularPlayerState.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ModularGameMode)
+
+AModularGameModeBase::AModularGameModeBase(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+AModularGameMode::AModularGameMode(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
