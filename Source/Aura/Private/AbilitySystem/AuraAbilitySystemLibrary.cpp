97d017e90856481af24f015109c4d6afaf9484e4
Peter Donald <peter@realityforge.org>
2025-05-18 21:16:27 +1000

    Start the BlueprintFunctionLibrary with access to the PlayerController

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,8 @@
+﻿#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "Player/AuraPlayerController.h"
+
+AAuraPlayerController* UAuraAbilitySystemLibrary::GetAuraPlayerController(const UObject* WorldContextObject)
+{
+    return Cast<AAuraPlayerController>(UGameplayStatics::GetPlayerController(WorldContextObject, 0));
+}
---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Player/AuraPlayerController.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+class UAttributeMenuWidgetController;
+class UOverlayWidgetController;
+
+UCLASS()
+class AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+    UFUNCTION(BlueprintPure,
+              Category = "AuraAbilitySystemLibrary",
+              meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject"))
+    static AAuraPlayerController* GetAuraPlayerController(const UObject* WorldContextObject);
+};
