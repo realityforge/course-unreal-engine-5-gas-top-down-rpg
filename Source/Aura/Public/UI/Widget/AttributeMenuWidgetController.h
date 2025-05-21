851dc6f7cd411be01e7b53283eb5d39adc53587b
Peter Donald <peter@realityforge.org>
2025-05-21 22:22:51 +1000

    Add delegate that can emit changes on attribute change

--- Source/Aura/Public/UI/Widget/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/Widget/AttributeMenuWidgetController.h
@@ -4,6 +4,11 @@
 #include "UI/Widget/AuraWidgetController.h"
 #include "AttributeMenuWidgetController.generated.h"
 
+class UAttributeInfo;
+struct FAuraAttributeInfo;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FAuraAttributeInfo&, AttributeInfo);
+
 UCLASS(Abstract, BlueprintType, Blueprintable)
 class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
 {
@@ -12,6 +17,9 @@ class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
 public:
     virtual void BroadcastInitialValues() override;
     virtual void BindCallbacksToDependencies() override;
+
+    UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+    FAttributeInfoSignature AttributeInfoDelegate;
 
 #if WITH_EDITOR
     virtual EDataValidationResult IsDataValid(FDataValidationContext& Context) const override;
