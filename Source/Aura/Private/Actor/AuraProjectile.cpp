a529f8a37a61499aed97bec67a2d6bf1fe42bbe4
Peter Donald <peter@realityforge.org>
2025-06-07 00:06:10 +1000

    Start to setup projectile

---
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -0,0 +1,41 @@
+#include "Actor/AuraProjectile.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AuraProjectile)
+
+AAuraProjectile::AAuraProjectile()
+{
+    // PrimaryActorTick.bCanEverTick = false;
+
+    Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+    Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+    Sphere->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+    Sphere->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+    Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+    SetRootComponent(Sphere);
+
+    ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
+    ProjectileMovement->InitialSpeed = 550.f;
+    ProjectileMovement->MaxSpeed = 550.f;
+    ProjectileMovement->ProjectileGravityScale = 0.f;
+}
+
+void AAuraProjectile::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraProjectile::OnSphereOverlap);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeStatic
+void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent,
+                                      AActor* OtherActor,
+                                      UPrimitiveComponent* OtherComp,
+                                      int32 OtherBodyIndex,
+                                      bool bFromSweep,
+                                      const FHitResult& SweepResult)
+{
+}
