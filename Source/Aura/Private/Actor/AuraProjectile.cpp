7e5a900fd764556a8b57cbdda9283546bf780b59
Peter Donald <peter@realityforge.org>
2025-06-15 22:00:46 +1000

    Suppress some annoying warnings

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -33,11 +33,12 @@ void AAuraProjectile::BeginPlay()
 }
 
 // ReSharper disable once CppMemberFunctionMayBeStatic
-void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent,
-                                      AActor* OtherActor,
-                                      UPrimitiveComponent* OtherComp,
-                                      int32 OtherBodyIndex,
-                                      bool bFromSweep,
-                                      const FHitResult& SweepResult)
+void AAuraProjectile::OnSphereOverlap([[maybe_unused]] UPrimitiveComponent* OverlappedComponent,
+                                      // ReSharper disable once CppParameterMayBeConstPtrOrRef
+                                      [[maybe_unused]] AActor* OtherActor,
+                                      [[maybe_unused]] UPrimitiveComponent* OtherComp,
+                                      [[maybe_unused]] int32 OtherBodyIndex,
+                                      [[maybe_unused]] bool bFromSweep,
+                                      [[maybe_unused]] const FHitResult& SweepResult)
 {
 }
